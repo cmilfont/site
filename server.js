8516ac01f51edbaeeee1a7cec4a258b231d56deb
Christiano Milfont <cmilfont@gmail.com>
2011-05-25 18:55:57 -0300

    nova versao do express

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express'),
   crypto = require('crypto');
 
@@ -21,5 +22,35 @@ app.get('/', function(req, res) {
 
 app.use(express.errorHandler({ showStack: true }));
 app.use(express.staticProvider(__dirname));
+
+app.listen(8001);
+*/
+
+var express = require('express'),
+  crypto = require('crypto');
+
+var app = express.createServer();
+
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.use(express.cookieParser());
+    app.use(express.static(__dirname));
+
+    app.use(express.session({ secret: 'milfont' }));
+
+    app.set('views', __dirname + '/views');
+    app.register('.html', require('ejs'));
+    app.set('view engine', 'html');
+    app.use(express.errorHandler({ showStack: true }));
+});
+
+
+app.get('/', function(req, res) {
+  var template = req.query['_escaped_fragment_'] || "home";
+  res.render(template.replace('/',''));
+});
+
 
 app.listen(8001);
