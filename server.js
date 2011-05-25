68cb08b1a7fcd1ef9c2f7bd30fa760e0d2d37f14
Christiano Milfont <cmilfont@gmail.com>
2011-05-25 19:08:41 -0300

    problema com a /

--- server.js
+++ server.js
@@ -49,10 +49,10 @@ app.configure(function(){
 
 app.get('/', function(req, res) {
   var template = req.query['_escaped_fragment_'] || "home";
-  if(template !== "home") {
+  /*if(template !== "home") {
      template = template.replace('/','');
-  }
-  res.render(template);
+  }*/
+  res.render(template + "/");
 });
 
 
